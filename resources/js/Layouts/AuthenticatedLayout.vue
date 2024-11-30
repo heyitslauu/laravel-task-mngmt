43d8594cc4bc0978e52bed7e61089d850c1ee41f
Laurence Cadag <laurencecadag1@gmail.com>
2024-11-30 13:04:34 +0800

    Authenticated Layout use RouterItem component

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
+
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
-import NavLink from "@/Components/NavLink.vue";
+import RouterItem from "@/Components/RouterItem.vue";
+
 import { Button } from "@/Components/ui/button";
 import { Input } from "@/Components/ui/input";
-import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/vue3";
-import { Bell, Home, ShoppingCart } from "lucide-vue-next";
-const showingNavigationDropdown = ref(false);
+import { Menu } from "lucide-vue-next";
+import { Sheet, SheetTrigger, SheetContent } from "@/Components/ui/sheet";
 </script>
 <template>
     <div
@@ -20,134 +21,47 @@ const showingNavigationDropdown = ref(false);
                 <div
                     class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6"
                 >
-                    <a href="/" class="flex items-center gap-2 font-semibold">
-                        <!-- <Package2 class="w-6 h-6" /> -->
+                    <Link
+                        href="/"
+                        class="flex items-center gap-2 font-semibold"
+                    >
                         <span class="">
                             <ApplicationLogo
                                 class="block w-auto fill-current h-9"
                         /></span>
-                    </a>
-                    <!-- <Button
-                        variant="outline"
-                        size="icon"
-                        class="w-8 h-8 ml-auto"
-                    >
-                        <Bell class="w-4 h-4" />
-                        <span class="sr-only">Toggle notifications</span>
-                    </Button> -->
+                    </Link>
                 </div>
                 <div class="flex-1">
                     <nav
                         class="grid items-start px-2 text-sm font-medium lg:px-4"
                     >
-                        <Link
-                            href="/"
-                            class="flex items-center gap-3 px-3 py-2 transition-all rounded-lg text-muted-foreground hover:text-primary"
-                        >
-                            <Home class="w-4 h-4" />
-                            Dashboard
-                        </Link>
+                        <RouterItem />
                     </nav>
                 </div>
-                <div class="p-4 mt-auto">
-                    <!-- <Card>
-              <CardHeader class="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent class="p-2 pt-0 md:p-4 md:pt-0">
-                <Button size="sm" class="w-full">
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card> -->
-                </div>
+                <div class="p-4 mt-auto"></div>
             </div>
         </div>
         <div class="flex flex-col">
             <header
                 class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6"
             >
-                <!-- <Sheet>
-            <SheetTrigger as-child>
-              <Button
-                variant="outline"
-                size="icon"
-                class="shrink-0 md:hidden"
-              >
-                <Menu class="w-5 h-5" />
-                <span class="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" class="flex flex-col">
-              <nav class="grid gap-2 text-lg font-medium">
-                <a
-                  href="#"
-                  class="flex items-center gap-2 text-lg font-semibold"
-                >
-                  <Package2 class="w-6 h-6" />
-                  <span class="sr-only">Acme Inc</span>
-                </a>
-                <a
-                  href="#"
-                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Home class="w-5 h-5" />
-                  Dashboard
-                </a>
-                <a
-                  href="#"
-                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                >
-                  <ShoppingCart class="w-5 h-5" />
-                  Orders
-                  <Badge class="flex items-center justify-center w-6 h-6 ml-auto rounded-full shrink-0">
-                    6
-                  </Badge>
-                </a>
-                <a
-                  href="#"
-                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Package class="w-5 h-5" />
-                  Products
-                </a>
-                <a
-                  href="#"
-                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <Users class="w-5 h-5" />
-                  Customers
-                </a>
-                <a
-                  href="#"
-                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                >
-                  <LineChart class="w-5 h-5" />
-                  Analytics
-                </a>
-              </nav>
-              <div class="mt-auto">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Upgrade to Pro</CardTitle>
-                    <CardDescription>
-                      Unlock all features and get unlimited access to our
-                      support team.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Button size="sm" class="w-full">
-                      Upgrade
-                    </Button>
-                  </CardContent>
-                </Card>
-              </div>
-            </SheetContent>
-          </Sheet> -->
+                <Sheet>
+                    <SheetTrigger as-child>
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            class="shrink-0 md:hidden"
+                        >
+                            <Menu class="w-5 h-5" />
+                            <span class="sr-only">Toggle navigation menu</span>
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left" class="flex flex-col">
+                        <nav class="grid gap-2 py-6 text-lg font-medium">
+                            <RouterItem />
+                        </nav>
+                    </SheetContent>
+                </Sheet>
                 <div class="flex-1 w-full">
                     <form>
                         <div class="relative">
@@ -200,22 +114,26 @@ const showingNavigationDropdown = ref(false);
                         </template>
                     </Dropdown>
                 </div>
-                <!-- <DropdownMenu>
-            <DropdownMenuTrigger as-child>
-              <Button variant="secondary" size="icon" class="rounded-full">
-                <CircleUser class="w-5 h-5" />
-                <span class="sr-only">Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu> -->
+                <DropdownMenu>
+                    <DropdownMenuTrigger as-child>
+                        <Button
+                            variant="secondary"
+                            size="icon"
+                            class="rounded-full"
+                        >
+                            <CircleUser class="w-5 h-5" />
+                            <span class="sr-only">Toggle user menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Settings</DropdownMenuItem>
+                        <DropdownMenuItem>Support</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </header>
             <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-6 lg:p-6">
                 <div class="flex items-center">
