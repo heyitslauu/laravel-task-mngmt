3cb9a2724b001413a700c2d7489722bf9ebfedd4
Laurence Cadag <laurencecadag1@gmail.com>
2024-11-30 09:52:54 +0800

    Rough Draft Layout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,21 +1,80 @@
 <script setup lang="ts">
-import { Head, Link } from "@inertiajs/vue3";
-import { Button } from "@/Components/ui/button";
-defineProps<{
-    canLogin?: boolean;
-    canRegister?: boolean;
-    laravelVersion: string;
-    phpVersion: string;
-}>();
+import { ref } from "vue";
+import AuthLayout from "@/Layouts/AuthLayout.vue";
 
-function handleImageError() {
-    document.getElementById("screenshot-container")?.classList.add("!hidden");
-    document.getElementById("docs-card")?.classList.add("!row-span-1");
-    document.getElementById("docs-card-content")?.classList.add("!flex-row");
-    document.getElementById("background")?.classList.add("!hidden");
-}
+import { Head, Link, useForm } from "@inertiajs/vue3";
+
+import { Input } from "@/Components/ui/input";
+import { Button } from "@/Components/ui/button";
+import { Checkbox } from "@/Components/ui/checkbox";
+import { Label } from "@/Components/ui/label";
+import InputError from "@/Components/InputError.vue";
+const form = useForm({
+    email: "",
+    password: "",
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route("login"), {
+        onFinish: () => {
+            form.reset("password");
+        },
+    });
+};
 </script>
 
 <template>
-    <h1>Test</h1>
+    <Head title="Welcome" />
+    <AuthLayout>
+        <form @submit.prevent="submit">
+            <div>
+                <Label for="email" value="Email">Email </Label>
+
+                <Input
+                    id="email"
+                    type="email"
+                    class="block w-full mt-1"
+                    v-model="form.email"
+                    required
+                    autofocus
+                    autocomplete="email"
+                />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <Label for="password" value="Password"> Password</Label>
+
+                <Input
+                    id="password"
+                    type="password"
+                    class="block w-full mt-1"
+                    v-model="form.password"
+                    required
+                    autocomplete="current-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="text-sm text-gray-600 ms-2">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Button
+                    class="ms-4"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                >
+                    Log in
+                </Button>
+            </div>
+        </form>
+    </AuthLayout>
 </template>
